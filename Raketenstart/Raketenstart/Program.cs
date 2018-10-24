5563c0324626dd7a8f9f179fa9ec75872f502959
Juan Rosenthal <Juan-rosenthal@outlook.com>
2018-10-25 01:37:45 +0200

    Add files via upload

---
+++ Raketenstart/Raketenstart/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\t\t\tRaketencountdown starten\n\n");
+            Console.ReadKey();
+
+            int i;
+
+            i = 10;
+
+            while (i > 0)
+            {
+                Console.WriteLine("\n\t\t\t\t"+i);
+                Thread.Sleep(1000);
+                Console.Beep();
+                i = i - 1;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.Clear();            
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");            
+            Console.WriteLine("\t\t\t     #");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t    ###");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #####");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t   #___#");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t  ##_#_##");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t ###_#_###");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t###########");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t    %%%");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t    %%%");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t     %");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);           
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("");
+            Thread.Sleep(200);
+            Console.WriteLine("\t\t\t\tMission Erfolgreich!\n\n\n\n\n\n\n");
+            Console.ReadKey();
+        }
+    }
+}
